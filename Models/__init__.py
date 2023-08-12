c4c39d44d2f6811d715bd3c6efd6b44530a58b9d
winniekiarie <kiariebsc@gmail.com>
2023-08-12 12:54:15 -0700

    init

---
+++ Models/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Package Initialization Script for the 'models' package.
+
+This script initializes the 'models' package by importing essential classes
+and setting up the FileStorage engine. It ensures proper configuration
+for collaborative development and data storage.
+"""
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
+
+storage = FileStorage()
+storage.relo
